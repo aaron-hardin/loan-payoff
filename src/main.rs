86e2ae4bdf912ee80d23e67f3b293b77db36237b
aaron-hardin <aaronrhardin@gmail.com>
2022-01-21 18:58:22 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <MyComponentWithProps prop1="lorem" prop2="ipsum" />
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Properties)]
+struct Props {
+    prop1: String,
+    prop2: String,
+}
+
+struct MyComponentWithProps;
+
+impl Component for MyComponentWithProps {
+    type Message = ();
+    type Properties = Props;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <span>
+                    { format!(
+                        "prop1: {} and prop2: {}",
+                        ctx.props().prop1,
+                        ctx.props().prop2
+                    ) }
+                </span>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
