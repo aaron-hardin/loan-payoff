7b398a27b22b84fb876359f6022c87dd2132f3c9
aaron-hardin <aaronrhardin@gmail.com>
2022-01-23 22:27:10 -0600

    Add display for Loan

--- loan-payoff/src/lib.rs
+++ loan-payoff/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Clone, PartialEq)]
 pub struct Loan {
     pub name: String,
@@ -12,5 +14,11 @@ impl Loan {
         self.present_value * (self.rate * f64::powf(1.0+self.rate, self.number_of_payments as f64))
         /
         (f64::powf(1.0+self.rate, self.number_of_payments as f64) - 1.0)
+    }
+}
+
+impl fmt::Display for Loan {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}, P={}, r={}, n={}, A={}", self.name, self.present_value, self.rate, self.number_of_payments, self.payment_amount)
     }
 }
